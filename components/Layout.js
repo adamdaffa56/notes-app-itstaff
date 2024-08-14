5d06a28e0ac49daac3caacabe046a727ab6335f6
adamdaffa56 <89517434+adamdaffa56@users.noreply.github.com>
2024-08-14 13:14:11 +0700

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,21 @@
-import { Box, Container } from "@chakra-ui/react";
+import { Box, Container, Heading } from "@chakra-ui/react";
 
 const layout = ({ children }) => (
   <Container maxW="container.md" py={4}>
-    <Box as="header" mb={8}>
-      <h1>Notes App</h1>
+    <Box
+      minHeight="10vh"
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      bgGradient="linear(to-r, teal.500, blue.500)"
+      color="white"
+      paddingTop={6}
+      marginBottom={6}
+    >
+      <Heading as="h1" size="2xl" mb={8} textAlign="center">
+        Notes App
+      </Heading>
     </Box>
     <main>{children}</main>
   </Container>
